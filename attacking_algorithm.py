5386159abacab0243a7ea76e874bdff19f0bca4e
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-04-12 23:08:33 +0200

    finish attacking

--- attacking_algorithm.py
+++ attacking_algorithm.py
@@ -1,18 +1,65 @@
 from rsa import RSA
 import sympy
+import time
+
+import matplotlib.pyplot as plt
 
 
-def attack_RSA():
-    rsa = RSA()
+def attack_RSA(E, N):
+
+    prime_factors = sympy.primefactors(N)
+    P = prime_factors[0]
+    Q = prime_factors[1]
+    # print("prime_factors : P =", P, " Q =", Q)
+
+    phin = (P-1)*(Q-1)
+    # print("Phi(N) =", phin)
+
+    D = pow(E, -1, phin)
+    return D
+
+
+def attack(start_prime, end_prime):
+    rsa = RSA(startPrime=start_prime, endPrime=end_prime)
     public_key = rsa.get_public_key()
-    print("public_key = ", public_key)
+    E = public_key[0]
+    N = public_key[1]
+    print("public_key : E =", E, " N =", N, " N =", bin(N))
+    start_time = time.time()
+    D = attack_RSA(E, N)
+    end_time = time.time()
 
-    prime_factors = sympy.primefactors(public_key[1])
-    print("prime_factors = ", prime_factors)
-
-    phin = (prime_factors[0]-1)*(prime_factors[1]-1)
-    d = pow(public_key[0], -1, phin)
-    print("private key = ", d)
+    print("private calculated key : D =", D)
+    print("Actual private key =", rsa.get_private_key())
+    return D, end_time - start_time
 
 
-attack_RSA()
+if __name__ == "__main__":
+    execution_time_list = []
+    number_of_bits_list = []
+    number_of_bits = 8
+    while number_of_bits <= 64:
+        number_of_bits_list.append(number_of_bits)
+        start_prime = int('1' + '0'*(number_of_bits-1), 2)
+        end_prime = int('1' + '1'*(number_of_bits-1), 2)
+        print("Number of bits =", number_of_bits)
+        D, execution_time = attack(start_prime, end_prime)
+        execution_time_list.append(execution_time)
+        print("Time to attack in milliseconds =", (execution_time)*1000, "ms")
+        print()
+        number_of_bits *= 2
+
+    # plot the execution time
+    # Create a figure and axis object
+    fig, ax = plt.subplots()
+
+    # Plot the data
+    ax.plot(number_of_bits_list, execution_time_list)
+
+    # Customize the plot
+    ax.set_title("Number of bits in N vs Execution time in milliseconds")
+    ax.set_xlabel("Number of bit in N")
+    ax.set_ylabel("Execution time in milliseconds")
+
+    # Display the plot
+    plt.show()
--- rsa.py
+++ rsa.py
@@ -3,27 +3,29 @@ import math
 
 
 class RSA:
-    def __init__(self):
-        print("Here")
+    def __init__(self, startPrime=100000, endPrime=10000000):
         self.P = 1  # sympy.randprime(100000, 1000000)  # 139273
         self.Q = 1  # sympy.nextprime(self.P)  # 139291
         self.E = 1  # 11
         self.D = 1  # 7054253411
-        self.generatePrimes()
+        self.generatePrimes(startPrime, endPrime)
         self.N = self.P * self.Q
         self.PHIN = (self.P - 1) * (self.Q - 1)
         self.generate_public_key()
         self.generate_private_key()
         print(self.P, self.Q, self.D, self.E, self.PHIN, self.N)
 
-    def generatePrimes(self):
-        self.P = sympy.randprime(100000, 1000000)
-        self.Q = sympy.randprime(100000, 1000000)
+    def generatePrimes(self, startPrime, endPrime):
+        self.P = sympy.randprime(startPrime, endPrime)
+        self.Q = sympy.randprime(startPrime, endPrime)
         while self.P == self.Q:
-            self.Q = sympy.randprime(100000, 1000000)
+            self.Q = sympy.randprime(startPrime, endPrime)
 
     def get_public_key(self):
         return (self.E, self.N)
+
+    def get_private_key(self):
+        return self.D
 
     def generate_public_key(self):
         for i in range(2, self.PHIN):
